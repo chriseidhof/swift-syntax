b2e830ffe32cbb2edfef541284e47af6408394a1
Chris Eidhof <chris@eidhof.nl>
2014-07-07 21:09:50 +0200

    Initial Commit

---
+++ highlight-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  highlight-swift
+//
+//  Created by Chris Eidhof on 07/07/14.
+//  Copyright (c) 2014 Chris Eidhof. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
